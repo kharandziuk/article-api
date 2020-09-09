fe21c5e6f4f0fcdaf640c3bb1bd88d4d702da77e
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-09-09 10:57:39 +0200

    ormconfig rds

--- ormconfig.js
+++ ormconfig.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 
 module.exports = {
    "type": "postgres",
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    username: process.env.DB_USERNAME || 'user',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'db',
+    host: process.env.DB_HOST || process.env.RDS_HOSTNAME || 'localhost',
+    port: process.env.DB_PORT || process.env.RDS_PORT || 5432,
+    username: process.env.DB_USERNAME || process.env.RDS_USERNAME || 'user',
+    password: process.env.DB_PASSWORD || process.env.RDS_PASSWORD || 'password',
+    database: process.env.DB_NAME || process.env.RDS_DB_NAME || 'db',
    "logging": true,
    "entities": ["dist/**/*.entity{ .ts,.js}"],
    "synchronize": false,
