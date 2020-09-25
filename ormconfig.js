632ae048dfea1ef733d5a5c09a92f0943af29522
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-09-25 18:00:52 +0200

    setup docker-compose

--- ormconfig.js
+++ ormconfig.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 
 module.exports = {
    "type": "postgres",
-    host: process.env.DB_HOST || process.env.RDS_HOSTNAME || 'localhost',
-    port: process.env.DB_PORT || process.env.RDS_PORT || 5432,
-    username: process.env.DB_USERNAME || process.env.RDS_USERNAME || 'user',
-    password: process.env.DB_PASSWORD || process.env.RDS_PASSWORD || 'password',
-    database: process.env.DB_NAME || process.env.RDS_DB_NAME || 'db',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
    "logging": true,
    "entities": ["dist/**/*.entity{ .ts,.js}"],
    "synchronize": false,
