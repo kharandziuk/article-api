520232c369cfd5bcd7c79cec8abf4e0b8e446d5d
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-10-01 11:46:22 +0200

    fix env variables for rds

--- ormconfig.js
+++ ormconfig.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 
 module.exports = {
    "type": "postgres",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT,
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_NAME,
    "logging": true,
    "entities": ["dist/**/*.entity{ .ts,.js}"],
    "synchronize": false,
